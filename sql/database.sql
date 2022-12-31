ffaf1ac8fca06d3cbc3fbec96a1f6c92ce51c14b
Samuel Banfi <samuel.banfi@samtrevano.ch>
2022-12-31 11:16:41 +0100

    Updated database

--- sql/database.sql
+++ sql/database.sql
@@ -2,16 +2,26 @@ DROP DATABASE IF EXISTS quiz_scuola_guida;
 CREATE DATABASE quiz_scuola_guida;
 USE quiz_scuola_guida;
 
+SET GLOBAL event_scheduler = ON;
+SET @@global.event_scheduler = ON;
+SET GLOBAL event_scheduler = 1;
+SET @@global.event_scheduler = 1;
+
 CREATE TABLE utente
 (
     email VARCHAR(30) PRIMARY KEY,
     nome VARCHAR(20) NOT NULL,
     cognome VARCHAR(20) NOT NULL,
     password VARCHAR(255) NOT NULL,
-    admin TINYINT(1) NOT NULL
+    admin TINYINT(1) NOT NULL,
+    data_creazione DATETIME DEFAULT CURRENT_TIMESTAMP
 );
 
 INSERT INTO utente VALUES("admin@gmail.com", "Admin", "istrator", "$2y$10$rt2kMEp4Ij2olyRLj2RA/uQ31zOTRbkNCF3fc7cPhAAQJjyYuIHOK", 1);
+INSERT INTO utente VALUES("gino@gmail.com", "Gino", "Admin", "$2y$10$rt2kMEp4Ij2olyRLj2RA/uQ31zOTRbkNCF3fc7cPhAAQJjyYuIHOK", 1);
+INSERT INTO utente VALUES("samuel@gmail.com", "Samuel", "Banfi", "$2y$10$rt2kMEp4Ij2olyRLj2RA/uQ31zOTRbkNCF3fc7cPhAAQJjyYuIHOK", 0, '2022-05-12 12:12:12');
+INSERT INTO utente VALUES("dennis@gmail.com", "Dennis", "Donofrio", "$2y$10$rt2kMEp4Ij2olyRLj2RA/uQ31zOTRbkNCF3fc7cPhAAQJjyYuIHOK", 0, '2022-06-13 15:15:15');
+INSERT INTO utente VALUES("damian@gmail.com", "Damian", "Campesi", "$2y$10$rt2kMEp4Ij2olyRLj2RA/uQ31zOTRbkNCF3fc7cPhAAQJjyYuIHOK", 0, '');
 
 CREATE TABLE domanda
 (
@@ -79,6 +89,41 @@ INSERT INTO `domanda` (`testo`, `risposta_1`, `risposta_2`, `risposta_3`, `rispo
 
 CREATE TABLE impostazioni
 (
-    limite_tempo INT NOT NULL,
-    limite_errori INT NOT NULL
+    limite_tempo INT NOT NULL DEFAULT -1,
+    limite_errori INT NOT NULL DEFAULT 3,
+    limite_accesso_utente INT NOT NULL DEFAULT 1
 );
+
+DELIMITER //
+CREATE PROCEDURE elimina_utente()
+BEGIN
+    DECLARE limite_accesso_utente DATETIME DEFAULT (SELECT limite_accesso_utente FROM impostazioni);
+    DECLARE utenti CURSOR FOR SELECT email,data_creazione,admin FROM utente;
+    DECLARE CONTINUE HANDLER FOR NOT FOUND SET finito = TRUE;
+
+    OPEN utenti;
+    loop_utenti: LOOP
+        FETCH utenti INTO email,data_eliminazione,admin;
+
+        IF finito THEN
+            LEAVE loop_utenti;
+        END IF;
+
+        IF admin = 1 THEN
+            CONTINUE loop_utenti;
+        END IF;
+
+        IF data_creazione + INTERVAL limite_accesso_utente MONTH < NOW() THEN
+            DELETE FROM utente WHERE email = email;
+        END IF;
+    END LOOP;
+    CLOSE utenti;
+END
+DELIMITER ;
+
+DELIMITER //
+CREATE EVENT evento_elimina_utente
+ON SCHEDULE EVERY 1 DAY
+DO
+    CALL elimina_utente();
+// DELIMITER ;
